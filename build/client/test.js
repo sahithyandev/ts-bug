df3b71b10ce7088e050c00693f3fde77752c5a9d
KSahithyan <sahithyan2701@gmail.com>
2020-07-02 09:12:42 +0530

    tsc upgraded to latest version

--- build/client/test.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var H = /** @class */ (function () {
-    function H() {
-    }
-    return H;
-}());
-exports.H = H;
--- build/index.js
+++
@@ -1,3 +0,0 @@
-"use strict";
-var Client = require('./client/client');
-console.log('working');
